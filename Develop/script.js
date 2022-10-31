5225b6228271ec54e0c0a7c06a3a7444ade39224
devin <devinelliotgomez@gmail.com>
2022-10-31 18:50:26 -0500

    cleaned up local storage saving using jquery tags

--- Develop/script.js
+++ Develop/script.js
@@ -6,61 +6,61 @@ showDate.text(currentTime);
 
 
 $(".saveBtn").on("click", function () {
-    var savedText9 = document.getElementById('text9').value;
+    var savedText9 = $('#text9').val();
     localStorage.setItem('9text', savedText9)
 
-    var savedText10 = document.getElementById('text10').value;
+    var savedText10 = $('#text10').val();
     localStorage.setItem('10text', savedText10)
 
-    var savedText11 = document.getElementById('text11').value;
+    var savedText11 = $('#text11').val();
     localStorage.setItem('11text', savedText11)
 
-    var savedText12 = document.getElementById('text12').value;
+    var savedText12 = $('#text12').val();
     localStorage.setItem('12text', savedText12)
 
-    var savedText1 = document.getElementById('text1').value;
+    var savedText1 = $('#text1').val();
     localStorage.setItem('1text', savedText1)
 
-    var savedText2 = document.getElementById('text2').value;
+    var savedText2 = $('#text2').val();
     localStorage.setItem('2text', savedText2)
 
-    var savedText3 = document.getElementById('text3').value;
+    var savedText3 = $('#text3').val();
     localStorage.setItem('3text', savedText3)
 
-    var savedText4 = document.getElementById('text4').value;
+    var savedText4 = $('#text4').val();
     localStorage.setItem('4text', savedText4)
 
-    var savedText5 = document.getElementById('text5').value;
+    var savedText5 = $('#text5').val();
     localStorage.setItem('5text', savedText5)
 
 
 });
-    var saved9 = localStorage.getItem('9text');
-    document.getElementById('text9').value = saved9;
+    var output9 = localStorage.getItem('9text');
+    $('#text9').val(output9); 
 
-    var saved10 = localStorage.getItem('10text');
-    document.getElementById('text10').value = saved10;
+    var output10 = localStorage.getItem('10text');
+    $('#text10').val(output10); 
 
-    var saved11 = localStorage.getItem('11text');
-    document.getElementById('text11').value = saved11;
+    var output11 = localStorage.getItem('11text');
+    $('#text11').val(output11); 
 
-    var saved12 = localStorage.getItem('12text');
-    document.getElementById('text12').value = saved12;
+    var output12 = localStorage.getItem('12text');
+    $('#text12').val(output12); 
 
-    var saved1 = localStorage.getItem('1text');
-    document.getElementById('text1').value = saved1;
+    var output1 = localStorage.getItem('1text');
+    $('#text1').val(output1); 
 
-    var saved2 = localStorage.getItem('2text');
-    document.getElementById('text2').value = saved2;
+    var output2 = localStorage.getItem('2text');
+    $('#text2').val(output2); 
 
-    var saved3 = localStorage.getItem('3text');
-    document.getElementById('text3').value = saved3;
+    var output3 = localStorage.getItem('3text');
+    $('#text3').val(output3); 
 
-    var saved4 = localStorage.getItem('4text');
-    document.getElementById('text4').value = saved4;
+    var output4 = localStorage.getItem('4text');
+    $('#text4').val(output4); 
 
-    var saved5 = localStorage.getItem('5text');
-    document.getElementById('text5').value = saved5;
+    var output5 = localStorage.getItem('5text');
+    $('#text5').val(output5);
 
 
 
