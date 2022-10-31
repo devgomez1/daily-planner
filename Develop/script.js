c6e727b29b4431d704a0d3b2075c924a615a7661
devin <devinelliotgomez@gmail.com>
2022-10-31 17:34:12 -0500

    worked on saving the text input into local storage for while, finally looks like its getting to work

--- Develop/script.js
+++ Develop/script.js
@@ -1,11 +1,15 @@
-$(document).ready(function() {
+
 var exactHour = moment().format('LT'); 
 var currentTime = moment().format('LLLL');
 var showDate = $("#currentDay");
 showDate.text(currentTime);
 
-//console.log(currentTime)
 
+$(".saveBtn").on("click", function () {
+    var savedText9 = document.getElementById('text9').value;
+    localStorage.setItem('9text', savedText9)
 
-})
+});
+var saved9 = localStorage.getItem('9text');
+     document.getElementById('text9').value = saved9;
 
